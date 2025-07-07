30880cfe9ec6ac22756e7d8bce143bb53fa71dcc
volalexey <sempayfox@gmail.com>
2025-07-07 23:29:37 +0300

    Added simple console menu

--- Practice_task/Program.cs
+++ Practice_task/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Practice_task
+﻿using System.ComponentModel.Design;
+
+namespace Practice_task
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Menu();
+        }
+
+        private static void Menu()
+        {
+            int userChoice;
+            string optionsList =
+                " _________________________\n" +
+                "|                         |\n" +
+                "|  ---------------------  |\n" +
+                "| | 1. Add Product \t| |\n" +
+                "| | 2. Remove Product \t| |\n" +
+                "| | 3. Add Tip \t\t| |\n" +
+                "| | 4. Display Bill \t| |\n" +
+                "| | 5. Clear All \t| |\n" +
+                "| | 6. Save TO File \t| |\n" +
+                "| | 7. Load from File \t| |\n" +
+                "| | 0. Exit \t\t| |\n" +
+                "|  ---------------------  |\n" +
+                "|                         |\n" +
+                " _________________________\n";
+
+            do
+            {
+                Console.WriteLine(optionsList);
+
+                Console.Write("Enter your chioce: ");
+                userChoice = Convert.ToInt32(Console.ReadLine());//add error
+            } while (userChoice != 0);
         }
     }
 }
