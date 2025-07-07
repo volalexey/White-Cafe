c2c530c86969db368badbc43339bd8693ebee77e
volalexey <sempayfox@gmail.com>
2025-07-07 23:15:57 +0300

    Add project files.

---
+++ Practice_task/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
